66d99f3dc8f22d13ff546073f80f3ed158242471
Chidera Nwachukwu <chideranwachukwu32@gmail.com>
2025-11-29 16:12:53 +0100

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -12,6 +12,7 @@ import { RootStackParamList } from './src/types/navigation';
 import WelcomeScreen from './src/screens/WelcomeScreen';
 import LoginScreen from './src/screens/LoginScreen';
 import SignUpScreen from './src/screens/SignUpScreen';
+import ForgotPasswordScreen from './src/screens/ForgotPasswordScreen';
 
 // Main Screens
 import FeedScreen from './src/screens/FeedScreen';
@@ -147,6 +148,7 @@ export default function App() {
             <Stack.Screen name="Welcome" component={WelcomeScreen} />
             <Stack.Screen name="Login" component={LoginScreen} />
             <Stack.Screen name="SignUp" component={SignUpScreen} />
+            <Stack.Screen name="ForgotPassword" component={ForgotPasswordScreen} />
 
             {/* Main */}
             <Stack.Screen name="Feed" component={FeedScreen} />
